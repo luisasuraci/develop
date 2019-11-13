8011920aa60b48788ed6a118cfb06da65fa6f3e1
Luisa <luisasuraci>
2019-11-13 16:54:15 +0100

    clear

--- spring-cloud-eco-system/ConsumerService/src/main/java/com/example/manager/utilities/Constants.java
+++
@@ -1,8 +0,0 @@
-package com.example.manager.utilities;
-
-public class Constants {
-
-    public final static double CELSIUS_MIN_RANGE = 15;
-    public final static double CELSIUS_MAX_RANGE = 30;
-    public final static double PH_NEUTRO  = 5.5;
-}
--- spring-cloud-eco-system/UserService/src/main/java/com/springboot/basics/springbootsample/service/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.basics.springbootsample.service;
-
-import com.springboot.basics.springbootsample.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-
-}
